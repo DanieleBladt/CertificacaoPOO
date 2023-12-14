e6fb2dc16235788d5e570f87852633949a8251c3
Daniele Bladt <danibs.01@gmail.com>
2023-12-14 14:48:08 -0300

    Commit Inicial

---
+++ CertificacaoPOO.dpr
@@ -0,0 +1,14 @@
+program CertificacaoPOO;
+
+uses
+  Vcl.Forms,
+  uCertificacaoPOO in 'uCertificacaoPOO.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
